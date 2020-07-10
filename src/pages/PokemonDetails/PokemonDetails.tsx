5196de0ddba2d67119ef8e09ccb70aecdcf42217
Airán <airanschez0@gmail.com>
2020-07-10 14:55:09 +0100

    :art: Added pokeball spinner :art:

--- src/pages/PokemonDetails/PokemonDetails.tsx
+++ src/pages/PokemonDetails/PokemonDetails.tsx
@@ -1,43 +1,53 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import './styles.scss';
-import {ContentDetail} from '../../components/ContentDetail';
+import { ContentDetail } from '../../components/ContentDetail';
 import axios from 'axios';
-import {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import '../../assets/Pokeball/pokeball-style.css';
+import { Pokemon } from './types';
 
 interface PokeProps {
-    searchTerm: string;
+  searchTerm: string;
 }
 
 export const PokemonDetails: React.FC<PokeProps> = () => {
-        const [pokemon, setPokemon] = useState('');
-        const {PokemonURL: urlParam} = useParams();
-        const [flag, setFlag] = useState(false);
+  const { PokemonURL: urlParam } = useParams();
+  const [flag, setFlag] = useState(false);
+  const [newPokemon, setNewPokemon] = useState<Pokemon>();
 
-        useEffect(() => {
-            try {
-                setFlag(true);
-                axios.get(`https://pokeapi.co/api/v2/pokemon/${urlParam.toLowerCase()}`)
-                    .then((response) => {
-                            setPokemon(response.data);
-                            setFlag(false);
-                        }
-                    );
-
-            } catch (error) {
-                console.log(error);
-            }
-        }, [urlParam]);
-
-        return <div className="PokemonDetails">
-            {flag ?
-                <div className="pokeball">
-                    <div className="pokeball__button"/>
-                </div>
-                : <ContentDetail pokemon={pokemon}/>}
-        </div>;
+  useEffect(() => {
+    try {
+      setFlag(true);
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${urlParam.toLowerCase()}`)
+        .then((response) => {
+          setNewPokemon((prevState) => ({
+            ...prevState,
+            name: response.data.name,
+            sprites: response.data.sprites,
+            stats: response.data.stats,
+          }));
+          setFlag(false);
+        });
+    } catch (error) {
+      console.log(error);
     }
-;
+  }, [urlParam]);
+
+  return (
+    <div className="PokemonDetails">
+      {flag ? (
+        <div className="pokeball">
+          <div className="pokeball__button" />
+        </div>
+      ) : newPokemon !== undefined ? (
+        <ContentDetail pokemon={newPokemon} />
+      ) : (
+        <div>me mori</div>
+      )}
+    </div>
+  );
+};
 
 PokemonDetails.displayName = 'PokemonDetails';

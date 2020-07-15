5e92ecbaa2f84d3f3300e16297e1967e6e06cfab
Airán <airanschez0@gmail.com>
2020-07-15 14:36:40 +0100

    :white_check_mark:Added simple tests

--- src/components/ContentDetail/ContentDetail.spec.tsx
+++ src/components/ContentDetail/ContentDetail.spec.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
-import { render, RenderResult } from '@testing-library/react';
-import { ContentDetail} from './';
+import {render, RenderResult} from '@testing-library/react';
+import {ContentDetail} from './';
+import {Pokemon} from '../../pages/PokemonDetails/types';
 
 describe('ContentDetail', () => {
-  it('should display the default message', () => {
-    const renderResult: RenderResult = render(
-      <ContentDetail/>,
-    );
-    expect(renderResult.queryByText('Hello from ContentDetail!')).toBeTruthy();
-  });
+    it('should display the name of the Pokemon', () => {
+        const pokemon: Pokemon = {
+            stats: [],
+            name: 'Ditto',
+            sprites: {
+                front_default: '',
+            }
+        };
+        const renderResult: RenderResult = render(
+            <ContentDetail pokemon={pokemon}/>,
+        );
+        expect(renderResult.queryByText('Nombre: Ditto')).toBeTruthy();
+        expect(renderResult.queryByText('Nombre: Pikachu')).toBeFalsy();
+    });
 });

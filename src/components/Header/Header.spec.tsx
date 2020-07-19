2fb7fd979f6b08173b9eea19691a1bc21c953e4f
Airán <airanschez0@gmail.com>
2020-07-19 20:09:33 +0100

    :white_check_mark: Fixed test

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
-import { render, RenderResult } from '@testing-library/react';
-import { Header} from './index';
+import {render, RenderResult} from '@testing-library/react';
+import {Header} from './index';
+import {BrowserRouter} from 'react-router-dom';
 
 describe('Header', () => {
-  it('should display the default message', () => {
-    const renderResult: RenderResult = render(
-      <Header/>,
-    );
-    expect(renderResult.queryAllByText(/Pokemon/i)).toBeTruthy();
-  });
+    it('should display the default message', () => {
+        const renderResult: RenderResult = render(
+            <BrowserRouter>
+                <Header/>,
+            </BrowserRouter>
+        );
+        expect(renderResult.queryByText(/Home/i)).toBeTruthy();
+    });
 });
